3a54067718d7b0d98fbf37c50f7ea92d56f1e83b
Nicolas Haenni <mail@nicolashaenni.me>
2017-11-05 15:50:55 +0100

    Kapitel 11 von C-style Array zu C++-Vektor geändert

--- kapitel11/main.cpp
+++ kapitel11/main.cpp
@@ -1,4 +1,33 @@
 #include <iostream>
+#include <vector>
+
+// Prototypes of functions
+void aufgabe111();
+void swap_vector(std::vector <double> &x);
+
+int main(int argc, char *argv[]) {
+    aufgabe111();
+
+    std::vector<double> v(2);
+
+    for (int i = 0; i < 2; i++) {
+        std::cout << "a[" << i << "] = ";   // Frage nach Wert i
+        std::cin >> v[i];                   // Speichere Wert in Vector
+    }
+
+    std::cout << "Die Werte nach aufruf der Funktion: \n";
+    // Funktion für Vector
+    swap_vector(v);
+
+    std::cout << "Ausgabe des Vectors: " << std::endl;
+
+    for (int i = 0; i < 2; i++) {
+        std::cout << "a[" << i << "] = " << v[i] << std::endl;
+    }
+
+
+    return 0;
+}
 
 // Aufgabe 11.1
 void aufgabe111() {
@@ -18,29 +47,6 @@ void aufgabe111() {
 }
 
 // Aufgabe 11.2
-void swap(double (&x)[2]) {
-    std::reverse(std::begin(x), std::end(x));
-}
-
-int main(int argc, char *argv[]) {
-    aufgabe111();
-
-    // Aufgabe 11.2
-
-    double a[2];
-
-    for (int i = 0; i < 2; i++) {
-        std::cout << "a[" << i << "] = ";
-        std::cin >> a[i];
-    }
-
-    std::cout << "Die Werte nach aufruf der Funktion: \n";
-
-    swap(a);
-
-    for (int i = 0; i < 2; i++) {
-        std::cout << "a[" << i << "] = " << a[i] << std::endl;
-    }
-
-    return 0;
+void swap_vector(std::vector <double> &x) {
+    std::reverse(x.begin(), x.end());
 }
