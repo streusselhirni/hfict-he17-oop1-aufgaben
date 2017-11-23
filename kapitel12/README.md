e6c11a09269e48b5790526f7103cc95f873d54ab
Nicolas Haenni <mail@nicolashaenni.me>
2017-11-23 19:38:14 +0100

    Aufgabe 12.3 auf gelöst

--- kapitel12/README.md
+++ kapitel12/README.md
@@ -32,4 +32,36 @@ Tipp: Beachten Sie das Beispiel im Skript.
 
 Das Ziel: Kombination von Strings und Funktionen kennen lernen.
 
+## 12.3 Ceasar-Chiffre
+
+Die Cäsar Verschlüsselung ist eine der einfachsten Verschlüsselungsalgorithmen.
+Jeder Buchstabe im Wort wird um eine bestimmte Anzahl Stellen im Alphabet noch hinten geschoben.
+Nachdem das z erricht wurde, muss das Alphabet zum weiterzählen nocheinmal wiederholt werden.
+
+Ihre Aufgabe: Schreiben Sie ein Programm, welches einen Schlüssel und ein Wort einliest welches verschlüsselt
+und wieder entschlüsselt werden kann.
+
+Eingabedaten:
+
+* ein Wort
+* Schlüssel
+
+Das Programm soll je eine Funktion zum verschlüsseln und entschlüsseln des Wortes beinhalten.
+
+Bildschirmausgabe:
+```
+Schluessel eingeben: 6
+Wort eingeben: programmieren
+Der Schlüssel lautet: 6
+Der verschlüsselte Text lautet: vxumxgssokxkt
+Der entschlüsselte Text lautet: programmieren
+```
+
+Zeichnen Sie zuerst ein Struktogramm des Programms.
+
+Bedingung: Verwenden Sie einen char-Array zum Speichern des Wortes und eine Integer Variable zum
+Speichern des Schlüssels.
+
+Das Ziel: Funktionsaufrufe mit Adressen kennen lernen. Die ASCII Zeichen kennen lernen.
+
 [Hier geht es weiter mit Kapitel 13.]([Struktogramm]https://github.com/streusselhirni/hfict-he17-oop1-aufgaben/tree/master/kapitel13)
--- kapitel12/main.cpp
+++ kapitel12/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 unsigned long int zaehlen(std::string s);
 
+std::vector<char> crypt(std::vector<char> w, int s);
+
 int main() {
     // Variablen deklarieren
-    std::string one;
-    std::string two;
-    std::string together;
-    std::string s;
+    std::string       one("");
+    std::string       two("");
+    std::string       together("");
+    std::string       s("");
+    std::string       tmp_s;
+    int               schluessel(0);
+    std::vector<char> a_word;
+    std::vector<char> crypted;
 
     // Strings einlesen
     std::cout << "Geben Sie den 1. Teilstring ein: ";
@@ -22,14 +30,68 @@ int main() {
     std::cout << "\n";
     std::cout << "\n";
 
-    // Aufgabe 2
+    // Aufgabe 12.2
     std::cout << "Geben Sie einen weiteren String ein: ";
     std::getline(std::cin, s);
     std::cout << zaehlen(s);
+
+    std::cout << "\n";
+    std::cout << "\n";
+
+    // Aufgabe 12.3
+    std::cout << "Schluessel eingeben: ";
+    std::cin >> schluessel;
+    std::cin.ignore(); // Needed to flush newline out of buffer for getline to work
+
+    if (schluessel > 26) {
+        schluessel %= 26;
+    }
+
+    std::cout << "Wort eingeben: ";
+    std::getline(std::cin, tmp_s);
+    // Read input into vector
+    for (auto &c : tmp_s) {
+        a_word.push_back(c);
+    }
+    crypted = crypt(a_word, schluessel);
+    for (auto &c : crypted) {
+        std::cout << c;
+    }
+
     return 0;
 }
 
-// Funktion für Aufgabe 2
+// Funktion für Aufgabe 12.2
 unsigned long int zaehlen(std::string s) {
     return s.length();
+}
+
+// Funktion für Aufgabe 12.3
+std::vector<char> crypt(std::vector<char> a_word, int s) {
+    std::vector<char> tmp;
+
+    for (auto &c : a_word) {
+        // Convert to ASCII
+        auto ascii = static_cast<int>(c);
+        // Für Grossbuchstaben
+        if (ascii >= 65 && ascii <= 90) {
+            // Wenn der Wert über Z hinausgeht
+            if ((ascii + s) > 90) {
+                // Formel durch ausprobieren gefunden
+                ascii = ((ascii + s) % 90 + 64);
+            } else {
+                ascii += s;
+            }
+        }
+            // Für Kleinbuchstaben
+        else if (ascii >= 97 && ascii <= 122) {
+            if ((ascii + s) > 122) {
+                ascii = ((ascii + s) % 122 + 96);
+            } else {
+                ascii += s;
+            }
+        }
+        tmp.push_back(static_cast<char>(ascii));
+    }
+    return tmp;
 }
